50c4b5900cf18128098aafe5cc9b11110e3317a9
Matheusss <matheusb95@gmail.com>
2019-09-26 23:33:27 -0300

    header component + start integrations

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const request = require('request');
 const app = express();
 app.use(bodyParser.json());
+app.get('*', cors());
 
 async function getList(req, res) {
    request.get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`, (error, response, body) => {
